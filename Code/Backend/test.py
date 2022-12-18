1ceb8dac3ae5c82abfe08838fc89bd7c3226c7a3
Jonathan <40543169+jonathanli15@users.noreply.github.com>
2022-12-17 19:30:55 -0500

    Update test.py

--- Code/Backend/test.py
+++ Code/Backend/test.py
@@ -38,6 +38,7 @@ def traceabilityResult(source, target, targetFile, feedback, model, metric = Non
     return values
     
 def outputValues(model, valuesDict, outputThreshold, curFile, output):
+    print("hello")
     for key in valuesDict:
         if (float(valuesDict[key]) >= outputThreshold):
             print("Source File: ",key, "Target File: ", curFile, "Traceability: ",valuesDict[key])
